5fdada9edcb679bd1818b1778a04583e9b3ce629
Samuel Lopes Ribeiro <samuellopesri4@gmail.com>
2023-10-30 14:09:17 -0300

    Z

--- src/app/pages/sec-unimed/sec-unimed.component.ts
+++ src/app/pages/sec-unimed/sec-unimed.component.ts
@@ -15,22 +15,52 @@ import { FichaService } from 'src/app/ficha.service';
 })
 export class SecUnimedComponent {
   fichas: Observable<any> = new Observable<any>();
-  selectedItem: any 
-
+  public selectedItem: any 
+  c: any 
   constructor(
     private fichaDataService: DataFichaService,
     private fichaService: FichaService,
     private db: AngularFireDatabase,
      ) {
-      const codRef = this.db.list('ficha')
+      const codRef = this.db.list('dbMateriais')
+      const outroRef = this.db.list('ficha')
       console.log(codRef);
 
       codRef.valueChanges().subscribe(items => 
         {
           console.log(items);
-          
+          for (const item of items) {
+            this.selectedItem.Codigo = item
+            
+            
+          }
         })
+
+
+      
+
+      // codRef.valueChanges().subscribe(items => 
+      //   {
+      //     for (const key in items) {
+      //       if (Object.prototype.hasOwnProperty.call(items, key)) {
+      //         let element: any = items[key]
+      //         console.log(key, element[1]);
+              
+      //       }
+            
+      //     }
+      //     // this.selectedItem = this.fichaService.getItemByCode();
+      //   })
       }
+
+   
+  // showCode(code: number) {
+  //   this.selectedItem = this.fichaService.getItemByCode(code);
+
+  //   if (this.selectedItem) {
+  //     console.log(this.selectedItem);
+  //   }
+  // }
 
   ngOnInit() {
     this.fichas = this.fichaService.getAll();
